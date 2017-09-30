619bfefb42f56d7f46ffa8d87ad935bdf7209ce1
MikeTeixeira <teixeiramichael5@gmail.com>
2017-09-30 19:37:59 -0400

    back button fixed and styling finished

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ SC.initialize({ client_id: 'BLIKpIMvNL9As25CHX4l9xXLu7KuU5uF'  });
 function Jukebox(){
   this.playlist = [];
   this.currentSong = 0;
-  SC.get(`/users/19252023/tracks`).then((response) => {
+  SC.get("/users/19252023/tracks").then((response) => {
       let newResponse = response.map(function(data){
         return {
           id: data.id,
           title: data.title,
           artwork_url: data.artwork_url,
           created_at: data.created_at,
-          permalink_url: data.permalink_url
+          permalink_url: data.permalink_url,
         };
       });
       this.playlist.push( ...newResponse );
@@ -24,11 +24,10 @@ function Jukebox(){
 }
 
       let myJukebox = document.querySelector(".jukebox"),
-         elPlayer = document.querySelector("audio"), 
          elCurrentSong = document.querySelector(".track_name"),
-         elControls = document.getElementById("songControls") 
+         elPlaylist = document.querySelectorAll(".playlist"),
+         elControls = document.getElementById("songControls"), 
          elSongImage = document.querySelector(".track_img"),
-         elPauseContainer = document.querySelector(".pause_container")
          elRewind = document.getElementById("rewind"),
          elPlay = document.getElementById("play"),
          elPause = document.getElementById("pause"),
@@ -39,25 +38,33 @@ function Jukebox(){
 
 Jukebox.prototype = {
   back: function(){
-
+    this.currentSong --;
+    if(this.currentSong <= -1){
+      this.currentSong = this.playlist.length - 1;
+      return jukebox.play();
+    } else {
+      this.currentSong --;
+      return jukebox.play();
+    }
+    console.log(this.currentSong);
   },
   play: function(){
     //if there already is a song -> let it play
       let song =  this.playlist[this.currentSong];
-      if(song.player){
-        song.player.play();
+      if(song.response){
+        song.response.play();
         //if there is no song, grab from playlist and play a song;
       } else {
-        SC.stream('/tracks/' + song.id).then((player) => {
-        song.player = player;
-        player.play();
+        SC.stream('/tracks/' + song.id).then((response) => {
+        song.response = response;
+        response.play();
       });
     }
   },
   pause: function(){
     let song = this.playlist[this.currentSong];
-    if(song.player){
-      return song.player.pause();
+    if(song.response){
+      return song.response.pause();
     }
   },
   forward: function(){
@@ -70,38 +77,38 @@ Jukebox.prototype = {
     elSongImage.src = this.playlist[this.currentSong].artwork_url;
     elCurrentSong.innerHTML = this.playlist[this.currentSong].title;
     elCreatedAt.innerText = this.playlist[this.currentSong].created_at;
+    elPermaLink.href = this.playlist[this.currentSong].permalink_url;
     elPermaLink.innerText = this.playlist[this.currentSong].permalink_url;
-  }
+  },
+
 }
 
 let jukebox = new Jukebox();
 
 
+
+
 document.addEventListener("DOMContentLoaded", function(){
+
 
 
   document.getElementById("songControls").addEventListener("click", function(event){
 
-      if(event.target == elRewind){
+      if(event.target === elRewind){
         jukebox.back();
-      } else if(event.target == elPlay){
+        jukebox.display();
+      } else if(event.target === elPlay){
         jukebox.display();
         jukebox.play();
-      } else if(event.target == elPause){
+      } else if(event.target === elPause){
         jukebox.pause();
-      } else if(event.target == elFastForw){      
+      } else if(event.target === elFastForw){      
         jukebox.forward();
         jukebox.display();
 
+
       }
   });
-
-  elPause.addEventListener("mouseover", function(event){
-      event.target.style = "none";
-  });
-
-
-
 
 
 
